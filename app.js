a46d6a0839cdbf9793e82e2e33d0d7579e051c81
torey2008 <69597895+torey2008@users.noreply.github.com>
2020-08-13 09:57:48 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,106 @@
+var userChoice = prompt('Please enter Rock, Paper or Scissors.');
+
+userChoice = userChoice.toLowerCase();
+
+ 
+
+if (userChoice === 'rock' || userChoice === 'paper'
+
+    || userChoice === 'scissors') {
+
+    document.write('You picked: ' + userChoice + '<br />' )
+
+} else { 
+
+    document.write('Please pick something specified')
+
+}
+
+ 
+
+var computerPick = Math.random();
+
+var computerChoice = 'scissors';
+
+if (computerPick < 0.34) {
+
+    computerChoice = 'rock';
+
+} else if (computerPick <= 0.66 ) {
+
+   computerChoice = 'paper';
+
+}
+
+ 
+
+document.write('The computer picked: ' + computerChoice);    
+
+ 
+
+ var compare = function(choice1, choice2) {
+
+    if (choice1 === choice2) {
+
+        return 'It is a tie';
+
+    }
+
+    if (choice1 === 'rock') {
+
+        if (choice2 === 'paper') {
+
+            return 'The computer won!'
+
+        } else {
+
+            return 'You WON!!!'
+
+        }        
+
+    }
+
+ 
+
+    if (choice1 === 'paper') {
+
+        if (choice2 === 'scissors') {
+
+            return 'The computer won!';
+
+        } else {
+
+            return 'You WON!!!'
+
+        }    
+
+    }
+
+    
+
+    if (choice1 === 'scissors') {
+
+        if (choice2 === 'rock') {
+
+            return 'The computer won!';
+
+        } else {
+
+            return 'You WON!!!'
+
+        }
+
+    }
+
+}    
+
+ 
+
+var winner = compare(userChoice, computerChoice);
+
+document.write('<br />The winner is: ' + winner);
+
+ 
+
+ 
+
